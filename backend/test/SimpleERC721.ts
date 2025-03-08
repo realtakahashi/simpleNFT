d89bd8f35244418e9fd0d7bd830e2ba2a1c2ed7e
shin.takahashi <realtakahashi.0924@gmail.com>
2025-03-08 19:31:07 +0900

    Completed full coverage

--- backend/test/SimpleERC721.ts
+++ backend/test/SimpleERC721.ts
@@ -23,94 +23,59 @@ describe("SimpleERC721", function () {
       expect(await simepleNFT.symbol()).to.equal("TNT");
     });
 
-      it("Should set the right owner", async function () {
-        const { simepleNFT, owner } = await loadFixture(deploySimpleERC721Fixture);
+    it("Should set the right owner", async function () {
+      const { simepleNFT, owner } = await loadFixture(
+        deploySimpleERC721Fixture
+      );
 
-        expect(await simepleNFT.owner()).to.equal(owner.address);
-      });
-
-    //   it("Should receive and store the funds to lock", async function () {
-    //     const { lock, lockedAmount } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-
-    //     expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-    //       lockedAmount
-    //     );
+      expect(await simepleNFT.owner()).to.equal(owner.address);
+    });
   });
+  describe("Mint", function () {
+    it("Only owner can mint", async function () {
+      const { simepleNFT, owner, otherAccount } = await loadFixture(
+        deploySimpleERC721Fixture
+      );
+      await expect(
+        simepleNFT
+          .connect(otherAccount)
+          .mint(
+            owner.getAddress(),
+            "Test description",
+            "htts://test.com",
+            "TestName",
+            "ticketKind,right",
+            "accomodation,you can stay the domitory for 1 day"
+          )
+      ).to.be.rejected;
 
-  //   it("Should fail if the unlockTime is not in the future", async function () {
-  //     // We don't use the fixture here because we want a different deployment
-  //     const latestTime = await time.latest();
-  //     const Lock = await hre.ethers.getContractFactory("Lock");
-  //     await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-  //       "Unlock time should be in the future"
-  //     );
-  //   });
-  // });
+      await simepleNFT
+        .connect(owner)
+        .mint(
+          otherAccount.getAddress(),
+          "Test description",
+          "htts://test.com",
+          "TestName",
+          "ticketKind,right",
+          "accomodation,you can stay the domitory for 1 day"
+        );
+      expect(await simepleNFT.ownerOf(0)).to.be.equal(
+        await otherAccount.getAddress()
+      );
+      const metaData = await simepleNFT.getOnChainMetadata(0);
+      expect(metaData.description).equal("Test description");
+      expect(metaData.external_url).equal("htts://test.com");
+      expect(metaData.name).equal("TestName");
+      expect(metaData.attribute_name_comma_seprate_string).equal(
+        "ticketKind,right"
+      );
+      expect(metaData.attribute_value_comma_seprate_string).equal(
+        "accomodation,you can stay the domitory for 1 day"
+      );
 
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
+      expect(await simepleNFT.tokenURI(0)).to.be.equal(
+        "This ERC721 does not have tokenURI."
+      );
+    });
+  });
 });
